ddd4c7aa0896bd1fcbbca524d0d47015a9548fb3
stayi <vardeath@yandex.ru>
2018-10-01 23:31:31 +0300

    переделка логики 2

--- app/src/main/java/com/example/stayi/myapplication/FragmentOnClickListener.java
+++ app/src/main/java/com/example/stayi/myapplication/FragmentOnClickListener.java
@@ -12,7 +12,10 @@ public abstract class FragmentOnClickListener implements View.OnClickListener {
     private TextView[] ArrayOfTextViews;
     private RadioButton[] ArrayOfRadBtn;
 
-    public FragmentOnClickListener(int fragment_id, View view, int[] ArrIdOfTextViews, int[] ArrIdOfRadbuttons){
+    private boolean[] View_Select_Status;
+    private boolean[] View_Activation;
+
+    FragmentOnClickListener(int fragment_id, View view, int[] ArrIdOfTextViews, int[] ArrIdOfRadbuttons){
         FRAGMENT_ID = fragment_id;
         ArrayIdTextViews = ArrIdOfTextViews;
         ArrayIdOfRadiobuttons = ArrIdOfRadbuttons;
@@ -26,6 +29,14 @@ public abstract class FragmentOnClickListener implements View.OnClickListener {
         for (int i = 0; i < ArrIdOfRadbuttons.length; ++i){
             ArrayOfRadBtn[i] = view.findViewById(ArrIdOfRadbuttons[i]);
             ArrayOfRadBtn[i].setOnClickListener(this);
+        }
+
+        View_Select_Status = new boolean[ArrIdOfTextViews.length];
+        View_Activation = new boolean[ArrIdOfTextViews.length];
+
+        for (int i = 0; i < ArrIdOfTextViews.length; ++i) {
+            View_Select_Status[i] = true;
+            View_Activation[i] = true;
         }
     }
 
@@ -50,11 +61,101 @@ public abstract class FragmentOnClickListener implements View.OnClickListener {
         return ArrayOfRadBtn;
     }
 
-    int find_position_RadBtn_id(int ID){
-        int found_position = 0;
+    RadioButton get_RadBtn_by_ID(int ID){
+        return ArrayOfRadBtn[get_RadBtn_position_by_ID(ID)];
+    }
+
+    int get_RadBtn_position_by_ID(int ID){
+        int found_position = -1;
         for (int i = 0; i < ArrayIdOfRadiobuttons.length; ++i){
             if (ArrayIdOfRadiobuttons[i] == ID) {found_position = i; }
         }
         return found_position;
+    }
+
+    int get_view_position_by_ID(int ID){
+        int found_position = -1;
+        for (int i = 0; i < ArrayIdTextViews.length; ++i){
+            if (ArrayIdTextViews[i] == ID) {found_position = i; }
+        }
+        return found_position;
+    }
+
+    private int get_selected_view_position() {
+        int pos = 0;
+        for (int i = 0; i < View_Select_Status.length; ++i) {
+            if (View_Select_Status[i]) pos = i;
+        }
+        return pos;
+    }
+
+    void set_view_select(int ID){
+        int position = get_view_position_by_ID(ID);
+        for (int i = 0; i < View_Select_Status.length; ++i){
+            if (i == position && View_Activation[i]) {
+                View_Select_Status[i] = true;
+                ArrayOfTextViews[i].setBackgroundResource(R.drawable.textstyle_selected);
+            } else {
+                if (View_Activation[i]) {
+                    View_Select_Status[i] = false;
+                    ArrayOfTextViews[i].setBackgroundResource(R.drawable.textstyle);
+                } else {
+                    View_Select_Status[i] = false;
+                    ArrayOfTextViews[i].setBackgroundResource(R.drawable.textstyle_not_active);
+                }
+            }
+        }
+    }
+
+    private int get_view_ID_by_position(int pos){
+        return ArrayIdTextViews[pos];
+    }
+
+    public TextView get_Selected_View(){
+        return ArrayOfTextViews[get_selected_view_position()];
+    }
+
+    void set_view_activation_state(int ID, boolean state){
+        View_Activation[get_view_position_by_ID(ID)] = state;
+
+    }
+
+
+    private boolean get_view_activation_state(int pos){
+        return View_Activation[pos];
+    }
+
+    void increment_view_selected_position(){
+        int pos = get_selected_view_position();
+        int min_pos = 0;
+        int max_pos = (ArrayIdTextViews.length - 1);
+        int increment = 1;
+        View_Select_Status[pos] = false;
+        if ( (pos + increment) >  max_pos) pos = min_pos;
+        else {
+            if ( (pos + increment*2) <=  max_pos) {
+                if (!get_view_activation_state(pos + increment)) increment = increment + 1;
+            }
+            pos = pos + increment;
+        }
+        View_Select_Status[pos] = true;
+        set_view_select(get_view_ID_by_position(pos));
+    }
+
+    void decrement_view_selected_position(){
+        int pos = get_selected_view_position();
+        int min_pos = 0;
+        int max_pos = (ArrayIdTextViews.length - 1);
+        int increment = -1;
+        View_Select_Status[pos] = false;
+        if ( (pos + increment) <  min_pos) pos = max_pos;
+        else {
+            if ( (pos + increment*2) >=  min_pos) {
+                if (!get_view_activation_state(pos + increment)) increment = increment*2;
+            }
+            pos = pos + increment;
+        }
+        View_Select_Status[pos] = true;
+        set_view_select(get_view_ID_by_position(pos));
     }
 }
--- app/src/main/java/com/example/stayi/myapplication/MillSimpleSelectLogic.java
+++ app/src/main/java/com/example/stayi/myapplication/MillSimpleSelectLogic.java
@@ -1,43 +1,43 @@
 package com.example.stayi.myapplication;
 
 import android.view.View;
-import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MillSimpleSelectLogic extends FragmentOnClickListener {
 
-    private RadioButton[] Radbtnarr;
     public MillSimpleSelectLogic(int fragment_id, View view, int[] ArrIdOfTextViews, int[] ArrIdOfRadbuttons){
         super(fragment_id, view, ArrIdOfTextViews, ArrIdOfRadbuttons);
-        Radbtnarr = getArrayOfRadBtn();
+        set_view_activation_state(R.id.TW_n_rev, false);
+        set_view_activation_state(R.id.TW_m_feed_editor, false);
+        set_view_select(R.id.TW_Mill_Diameter);
     }
+
 
     @Override
     public void onClick(View v) {
         super.onClick(v);
-
         int id = v.getId();
-        for (int i = 0; i < getArrayIdTextViews().length; ++i) {
-            if (getArrayIdTextViews()[i] == id) getArrayOfTextViews()[i].setBackgroundResource(R.drawable.textstyle_selected);
-            else getArrayOfTextViews()[i].setBackgroundResource(R.drawable.textstyle);
+        int selected_view_position = get_view_position_by_ID(id);
+        if (selected_view_position >= 0) {
+            set_view_select(id);
         }
 
         switch (id){
             case R.id.fix_but_Vc:
-                getArrayOfRadBtn()[find_position_RadBtn_id(R.id.fix_but_Vc)].setChecked(true);
-                getArrayOfRadBtn()[find_position_RadBtn_id(R.id.fix_but_rev)].setChecked(false);
+                get_RadBtn_by_ID(R.id.fix_but_Vc).setChecked(true);
+                get_RadBtn_by_ID(R.id.fix_but_rev).setChecked(false);
                 break;
             case R.id.fix_but_rev:
-                getArrayOfRadBtn()[find_position_RadBtn_id(R.id.fix_but_Vc)].setChecked(false);
-                getArrayOfRadBtn()[find_position_RadBtn_id(R.id.fix_but_rev)].setChecked(true);
+                get_RadBtn_by_ID(R.id.fix_but_Vc).setChecked(false);
+                get_RadBtn_by_ID(R.id.fix_but_rev).setChecked(true);
                 break;
             case R.id.fix_but_fz:
-                getArrayOfRadBtn()[find_position_RadBtn_id(R.id.fix_but_fz)].setChecked(true);
-                getArrayOfRadBtn()[find_position_RadBtn_id(R.id.fix_but_F)].setChecked(false);
+                get_RadBtn_by_ID(R.id.fix_but_fz).setChecked(true);
+                get_RadBtn_by_ID(R.id.fix_but_F).setChecked(false);
                 break;
             case R.id.fix_but_F:
-                getArrayOfRadBtn()[find_position_RadBtn_id(R.id.fix_but_fz)].setChecked(false);
-                getArrayOfRadBtn()[find_position_RadBtn_id(R.id.fix_but_F)].setChecked(true);
+                get_RadBtn_by_ID(R.id.fix_but_fz).setChecked(false);
+                get_RadBtn_by_ID(R.id.fix_but_F).setChecked(true);
                 break;
         }
     }
