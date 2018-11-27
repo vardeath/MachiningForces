6c0f800e70b3a70778121572307e83c792cc7b58
stayi <vardeath@yandex.ru>
2018-11-27 22:35:39 +0300

    начал переделывать рехолд полей ввода

---
+++ app/src/main/java/Fragment_data.java
@@ -0,0 +1,7 @@
+public class Fragment_data {
+    int TextView_id;
+    String TextView_string_value;
+    Double TextView_double_value;
+    boolean selected_state;
+    boolean allowed_to_select;
+}
---
+++ app/src/main/java/com/example/stayi/myapplication/Button_hold_adaptor.java
@@ -0,0 +1,8 @@
+package com.example.stayi.myapplication;
+
+public class Button_hold_adaptor {
+
+    Button_hold_adaptor(int[] TextViews, int button_id, int first_text_field, int second_text_field, int hold_position) {
+
+    };
+}
